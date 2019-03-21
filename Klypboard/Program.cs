b32f7d887c3c034d5537873dad3c0ecce809e6a0
Kyle Carson <carsonkb@nih.gov>
2019-03-21 16:23:10 -0400

    Add project files.

---
+++ Klypboard/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Klypboard
+{
+    using System;
+    using System.Windows.Forms;
+
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var displayText = string.Empty;
+
+            while (true)
+            {
+                var clipboardText = string.Empty;
+
+                try
+                {
+                    clipboardText = Clipboard.GetText(TextDataFormat.Text);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                
+                if (!string.IsNullOrEmpty(clipboardText))
+                {
+                    if (displayText != clipboardText)
+                    {
+                        displayText = clipboardText;
+
+                        Console.WriteLine($"\n\n{DateTime.Now}\n\n{displayText}");
+                    }
+                }
+            }
+        }
+    }
+}
